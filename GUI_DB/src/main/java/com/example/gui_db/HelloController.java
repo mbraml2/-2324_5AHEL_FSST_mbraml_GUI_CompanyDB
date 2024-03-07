c6ad2b31ea4c77dedb4313e2f618ef5df185cf11
mbraml2 <mbraml@htl-steyr.ac.at>
2024-03-07 09:24:06 +0100

    Interface added

--- GUI_DB/src/main/java/com/example/gui_db/HelloController.java
+++ GUI_DB/src/main/java/com/example/gui_db/HelloController.java
@@ -7,8 +7,8 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    //@FXML
+    //protected void onHelloButtonClick() {
+      //  welcomeText.setText("Welcome to JavaFX Application!");
+    //}
 }
